3627779649ec66177d70dba7b788314d7c9d692e
Rakesh Mandal <rkm8448746347@gmail.com>
2023-11-25 22:39:05 +0530

    frontend part of social media app for deployment on netlify

--- backend/Routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const { signUp, login } = require("../controllers/authController")
-const { validateSignup } = require("../middlewares/authMiddleware")
-
-
-module.exports = (app) => {
-    
-    app.post("/auth/signup",validateSignup,signUp)
-    app.post("/auth/login",login)
-}
